1d7babaa78ddb4bffd0db2a560e8cef12fd72426
Aryan <61783446+Aryan-B@users.noreply.github.com>
2020-08-02 22:18:41 +0530

    updated projects

---
+++ Aryan.social/backend/connection.php
@@ -0,0 +1,27 @@
+<?php
+
+	$dbServername= ' removed for security purposes ';
+	$dbUsername=  ' removed for security purposes ';
+	$dbPassword=  ' removed for security purposes ';
+	$dbName=  ' removed for security purposes ';
+
+	$conn = new mysqli($dbServername,$dbUsername,$dbPassword,$dbName);
+
+	if ($conn->connect_error) {
+		echo "unable to connect to server";
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?> 
